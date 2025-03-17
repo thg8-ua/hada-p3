437a84a4726f80b255493bbd3022e64c71d41257
thg8-ua <thg8@alu.ua.es>
2025-03-17 15:45:51 +0100

    54795313K Terminar el Site1.Master y Default.aspx

---
+++ proWeb/Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="proWeb.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="formContent" runat="server">
+    <p class="subtitle">Product Management</p>
+        <div>
+            <p>
+            Code <asp:TextBox id="code" TextMode="SingleLine" Columns="38" runat="server" style="position: absolute"/>
+            </p>
+            <p>
+                Name <asp:TextBox id="name" TextMode="SingleLine" Columns="40" runat="server" style="position: absolute"/>
+            </p>
+            <p>
+                Amount <asp:TextBox ID ="amount" TextMode="SingleLine" Columns="20" runat="server" style="position: absolute"/>
+            </p>
+            <p>
+                Category <asp:DropDownList id="category" runat="server" style="position: absolute" OnSelectedIndexChanged="category_SelectedIndexChanged">
+                    <asp:ListItem Selected="True" Value="computing">Computing</asp:ListItem>
+                    <asp:ListItem Value="telephony">Telephony</asp:ListItem>
+                    <asp:ListItem Value="gaming">Gaming</asp:ListItem>
+                    <asp:ListItem Value="homeapps">Home appliances</asp:ListItem>
+                    </asp:DropDownList>
+            </p>
+            <p>
+                Price <asp:TextBox ID ="price" TextMode="SingleLine" Columns="16" runat="server" style="position: absolute"/>
+            </p>
+            <p>
+                Creation Date <asp:TextBox ID ="date" TextMode="SingleLine" Columns="30" runat="server" style="position: absolute"/>
+            </p>
+
+            <div>
+            <p> <!-- figure how to indent without spamming nbsp later-->
+                <asp:Button id="create" Text="Create" runat="server" />
+                &nbsp&nbsp&nbsp
+                <asp:Button id="update" Text="Update" runat="server" />
+                &nbsp&nbsp&nbsp
+                <asp:Button id="delete" Text="Delete" runat="server" />
+                &nbsp&nbsp&nbsp
+                <asp:Button id="read" Text="Read" runat="server" />
+                &nbsp&nbsp&nbsp
+                <asp:Button id="readfirst" Text="Read First" runat="server" />
+                &nbsp&nbsp&nbsp
+                <asp:Button id="readprev" Text="Read Prev" runat="server" />
+                &nbsp&nbsp&nbsp
+                <asp:Button id="readnext" Text="Read Next" runat="server" />
+            </p>
+            </div>
+        </div>
+</asp:Content>
---
+++ proWeb/Default.aspx.designer.cs
@@ -0,0 +1,134 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace proWeb
+{
+
+
+    public partial class Default
+    {
+
+        /// <summary>
+        /// Control code.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox code;
+
+        /// <summary>
+        /// Control name.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox name;
+
+        /// <summary>
+        /// Control amount.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox amount;
+
+        /// <summary>
+        /// Control category.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList category;
+
+        /// <summary>
+        /// Control price.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox price;
+
+        /// <summary>
+        /// Control date.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox date;
+
+        /// <summary>
+        /// Control create.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button create;
+
+        /// <summary>
+        /// Control update.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button update;
+
+        /// <summary>
+        /// Control delete.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button delete;
+
+        /// <summary>
+        /// Control read.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button read;
+
+        /// <summary>
+        /// Control readfirst.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button readfirst;
+
+        /// <summary>
+        /// Control readprev.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button readprev;
+
+        /// <summary>
+        /// Control readnext.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button readnext;
+    }
+}
