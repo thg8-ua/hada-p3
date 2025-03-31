7286bc23beb08db4bdb469daeb7bed7d819133c7
thg8-ua <thg8@alu.ua.es>
2025-03-31 16:59:35 +0200

    54795313K Hacer el BD y empezar con crear clase CADProduct y ENProduct

---
+++ library/CADProduct.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    public class CADProduct
+    {
+        private string cadena;
+
+        public CADProduct()
+        {
+            this.cadena = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
+        }
+
+        public bool Create(ENProduct en)
+        {
+            SqlCommand com;
+            SqlConnection conn;
+            bool success = false;
+            string query = "INSERT INTO Products(code, name, amount, category, price, creationDate) VALUES (@code, @name, @amount, @category, @price, @creationDate)";
+
+            try
+            {
+                conn = new SqlConnection = new SqlConnection(cadena);
+                conn.Open();
+                com = new SqlCommand(query, conn);
+                com.Parameters.AddWithValue("@code", en.code);
+                com.Parameters.AddWithValue("@name", en.name);
+                com.Parameters.AddWithValue("@amount", en.amount);
+                com.Parameters.AddWithValue("@price", en.price);
+                com.Parameters.AddWithValue("@category", en.category);
+                com.Parameters.AddWithValue("@creationDate", en.creationDate);
+
+                int rowAffect = com.ExecuteNonQuery();
+                success = rowAffect > 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"ERROR: DIPSHIT {e.Message}");
+            }
+            finally {
+                if (com != null)  // WHAT DO YOU MEAN ITS WITHIN THE FUCKING SCOPE
+                { 
+                    com.Dispose();
+                    conn.Close();
+                }
+               
+            }
+            return success;
+        }    
+    }
+}
---
+++ library/ENProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    internal class ENProduct
+    {
+        //TODO: create the function asshole
+        private string _code;
+        private string _name;
+        private int _amount;
+        private float _price;
+        private int _category;
+        private DateTime _creationDate;
+
+        public string code { get { return _code; } set { _code = value; } }
+        public string name { get { return _name;    } set { _name = value; } }
+        public int amount { get { return _amount;   } set { _amount = value; } }
+        public float price { get { return _price;   } set { _price = value; } } 
+        public int category { get { return _category;   } set { _category = value; } }
+        public DateTime creationDate { get { return _creationDate;  } set { _creationDate = value; }
+
+    }
+}
